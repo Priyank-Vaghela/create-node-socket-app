bb5c514cda3a3df077af00fda0f08482eebd46b3
Priyank-Vaghela <priyankvaghela11@gmail.com>
2020-02-20 12:24:30 +0000

    Initial commit

---
+++ src/index2.js
@@ -0,0 +1,26 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+redisAdapter = require("socket.io-redis");
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index2.html");
+});
+
+io.adapter(redisAdapter({ host: "3.83.47.185", port: 6379 }));
+io.on("connection", function(socket) {
+  console.log("user connected: " + socket.id);
+  io.emit("hi", "connect user id:" + socket.id);
+
+  socket.on("disconnect", function() {
+    console.log("Got disconnect: " + socket.id);
+  });
+  socket.on("chat message", function(msg, fn) {
+    console.log("message: " + msg);
+    fn(msg + " received  ");
+  });
+});
+
+http.listen(3002, function() {
+  console.log("listening on *:3002");
+});
